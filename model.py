f9035b24197db1f017074dd9d0f9f5fcd258e322
psh150204 <psh131226@gmail.com>
2020-07-22 14:22:11 +0000

    add doubly stochastic attention

--- model.py
+++ model.py
@@ -33,6 +33,8 @@ class Attention(nn.Module):
         alpha = F.softmax(e, dim = -1) # [batch, L]
         z = torch.sum(a * alpha.unsqueeze(2), dim = 1) # [batch, D]
 
+        return z, alpha
+
 class Decoder(nn.Module):
     def __init__(self, a_dim, h_dim, attn_dim, vocab_size, embed_dim):
         super(Decoder, self).__init__()
@@ -47,11 +49,14 @@ class Decoder(nn.Module):
         self.f_h = nn.Linear(h_dim, embed_dim, bias = False)
         self.f_z = nn.Linear(a_dim, embed_dim, bias = False)
 
+        # doubly stochastic attention
+        self.f_beta = nn.Linear(h_dim, a_dim)
+
     def forward(self, a, trg):
         # param a : encoder output (a tensor with size [batch, L, a_dim])
         # param trg : target caption (a tensor with size [batch, C])
         max_caption_len = trg.size(1)
-        pred = None
+        pred, alpha = None, None
 
         h_t, c_t = self.f_init_h(torch.mean(a, dim = 1)), self.f_init_c(torch.mean(a, dim = 1))
         z_t = self.attention(a, h_t) # [batch, a_dim]
@@ -60,21 +65,30 @@ class Decoder(nn.Module):
         for t in range(max_caption_len):
             # main procedure
             Ey_t = self.embedding(y_t) # [batch, 1, embed_dim]
+            beta = F.sigmoid(self.f_beta(h_t)) # [batch, h_dim] -> [batch, a_dim]
+            
             lstm_input = torch.cat([Ey_t, z_t.unsqueeze(1)], dim = -1) # [batch, 1, a_dim + embed_dim]
             h_t, c_t = self.LSTM(lstm_input, (h_t, c_t)) # [batch, 1, h_dim], [batch, 1, h_dim]
-            z_t = self.attention(a, h_t) # [batch, a_dim]
+            z_t, alpha_t = self.attention(a, h_t) # [batch, a_dim], [batch, L]
+
+            # doubly stochastic attention
+            z_t = beta * z_t
             
             # Deep output layer
             output = self.f_out(Ey_t.squeeze(1) + self.f_h(h_t.squeeze(1)) + self.f_z(z_t)) # [batch, vocab_size]
-            prob = F.softmax(output, dim = -1) # [batch, vocab_size]
+            
+            #prob = F.softmax(output, dim = -1) # [batch, vocab_size]
+            prob = output # exclude softmax to use CrossEntropyLoss
 
             if pred is None:
                 pred = prob.unsqueeze(1) # [batch, 1, vocab_size]
+                alpha = alpha_t.unsqueeze(1) # [batch, 1, a_dim]
             else:
                 pred = torch.cat([pred, prob.unsqueeze(1)], dim = 1) # [batch, t+1, vocab_size]
+                alpha = torch.cat([alpha, alpha_t.unsqueeze(1)], dim = 1) # [batch, t+1, a_dim]
             
             # next word
             if t < max_caption_len - 1:
                 y_t = trg[:,t+1:t+2] # teacher forcing
         
-        return pred # [batch, C, voca_size]
+        return pred, alpha # [batch, C, vocab_size], [batch, C, L]
